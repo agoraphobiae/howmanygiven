edaefe323caf27c5cf7ead60faaea5f5980448e7
Kevin Lee <kevinmlee.x@gmail.com>
2013-11-24 00:37:38 -0800

    added URL debug print() call

--- howmanyapp/howmany.py
+++ howmanyapp/howmany.py
@@ -4,6 +4,7 @@ import mechanize
 import re
 
 def countInPage(url, s):
+	print "URL: ", url
 	br = mechanize.Browser()
 	br.addheaders = [('User-Agent',
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36')]
